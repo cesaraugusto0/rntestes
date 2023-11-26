e2ba8c896cb99e51f537320f6acf014c97661301
Cesar Augusto <augustocsar22@gmail.com>
2023-11-26 19:50:26 -0300

    testando um hook

---
+++ __tests__/hooks/useListaLeiloes.test.js
@@ -0,0 +1,43 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useListaLeiloes from '../../src/hooks/useListaLeiloes';
+import { obtemLeiloes } from '../../src/repositorio/leilao';
+
+jest.mock('../../src/repositorio/leilao');
+
+const mockLeiloes = [
+    {
+        id: 1,
+        nome: 'Leilão',
+        descricao: 'Descrição do leilao'
+    }
+];
+
+const mockLeiloesAtualizada = [
+    {
+        id: 1,
+        nome: 'Leilão',
+        descricao: 'Descrição do leilao'
+    },
+    {
+        id: 2,
+        nome: 'Leilão 2',
+        descricao: 'Descrição do leilao 2'
+    }
+];
+
+describe('hooks/useListaLeiloes',() => {
+
+    it('deve retornar uma lista de leilões e uma função para atualizar', async () => {
+        obtemLeiloes.mockImplementation(() => mockLeiloes);
+        const { result, waitForNextUpdate } = renderHook(() => useListaLeiloes());
+        
+        expect(result.current[0]).toEqual([]);
+        await waitForNextUpdate();
+        expect(result.current[0]).toEqual(mockLeiloes);
+
+        obtemLeiloes.mockImplementation(() => mockLeiloesAtualizada);
+
+        await act(() => result.current[1]());
+        expect(result.current[0]).toEqual(mockLeiloesAtualizada);
+    });
+});
